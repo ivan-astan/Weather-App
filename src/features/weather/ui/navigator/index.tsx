e74516ceeec568e2f3873bdd9b4936e3e0473e47
Ivan Astanin <ivanastanin@MacBook-Air-Ivan.local>
2024-10-30 19:02:43 +0300

    fix

--- src/features/weather/ui/navigator/index.tsx
+++ src/features/weather/ui/navigator/index.tsx
@@ -1,26 +1,20 @@
 import { useState } from "react";
 import classes from "./navigator.module.css";
 export const Navigator = () => {
-  const [active, setActive] = useState("Today");
+  const [active, setActive] = useState("Weather");
   return (
     <nav className={classes.nav}>
       <div
-        onClick={() => setActive("Today")}
-        className={active === "Today" ? classes.active : ""}
+        onClick={() => setActive("Weather")}
+        className={active === "Weather" ? classes.active : ""}
       >
         Today
       </div>
       <div
-        onClick={() => setActive("Tommorow")}
-        className={active === "Tommorow" ? classes.active : ""}
+        onClick={() => setActive("News")}
+        className={active === "News" ? classes.active : ""}
       >
-        Tommorow
-      </div>
-      <div
-        onClick={() => setActive("Monthly")}
-        className={active === "Monthly" ? classes.active : ""}
-      >
-        Monthly Forecast
+          News
       </div>
     </nav>
   );
